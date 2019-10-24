dce0ad568ad088d90136792c5c801115e58a67ea
micchel <msaito2112@gmail.com>
2019-10-24 19:06:12 +0900

    set application.rb not to create unnecessary files
    when create controller

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,12 @@ module Graphy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework false
+      g.template_engine false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
