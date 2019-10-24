f038d10334c9353a6697182e644424860c7e1631
micchel <msaito2112@gmail.com>
2019-10-24 19:15:33 +0900

    set timezone to tokyo

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module Graphy
     end
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.time_zone = 'Tokyo'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
