731a0a36ba9107557d04aa89e711771bc5808fb6
micchel <msaito2112@gmail.com>
2019-11-11 15:17:42 +0900

    Edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root 'top#index'
   resources :top, only: :index
+  resources :photographers do
+    collection do
+      get 'match'
+    end
+  end
+  resources :users
+  resources :photographer_chat_rooms
 end
