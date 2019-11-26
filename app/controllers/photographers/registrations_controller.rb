80f73007452c011506159098c79af2398dc27c9e
micchel <msaito2112@gmail.com>
2019-11-26 19:14:48 +0900

    edit devise controller

--- app/controllers/photographers/registrations_controller.rb
+++ app/controllers/photographers/registrations_controller.rb
@@ -3,15 +3,15 @@
 class Photographers::RegistrationsController < Devise::RegistrationsController
   include Accessible
   skip_before_action :check_user, except: [:new, :create]
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
-
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
+  before_action :authenticate_photographer!, only: [:edit]
   # GET /resource/sign_up
   # def new
   #   super
   # end
 
-  # POST /resource
+  # # POST /resource
   # def create
   #   super
   # end
@@ -43,19 +43,27 @@ class Photographers::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name, :sex_type, :facebook_url, :instagram_url, :twitter_url, :price, :self_introduction, :avatar, prefecture_ids: []])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys:[:name, :sex_type, :facebook_url, :instagram_url, :twitter_url, :price, :self_introduction, :avatar, prefecture_ids: []])
+  end
+
+  def update_resource(resource, configure_account_update_params)
+    resource.update_without_password(configure_account_update_params)
+  end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    authenticated_photographer_root_path
+  end
+
+  def after_update_path_for(resource)
+    authenticated_photographer_root_path
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,8 +3,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   include Accessible
   skip_before_action :check_user, except: [:new, :create]
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
+  before_action :authenticate_user!, only: [:edit]
 
   # GET /resource/sign_up
   # def new
@@ -40,22 +41,26 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name, :sex_type, :prefecture_id, :self_introduction, :avatar, [:attribute]])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys:[:name, :sex_type, :prefecture_id, :self_introduction, :avatar ,[:attribute]])
+  end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    authenticated_user_root_path
+  end
+
+  def after_update_path_for(resource)
+    authenticated_user_root_path
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
