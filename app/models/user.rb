b56aeaeed3f833b20da758dd34902d4c95297f46
micchel <msaito2112@gmail.com>
2019-11-26 20:06:49 +0900

    create model
    user
    photographer
    prefecture
    photographer_prefecture

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
+  
+  belongs_to :prefecture
+
+  enum sex: { male:1, female:2 }
+  mount_uploader :avatar, ImageUploader
 end
