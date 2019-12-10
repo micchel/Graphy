61a776550e2cb774cc8475f9413cff117105df10
micchel <msaito2112@gmail.com>
2019-12-10 18:39:25 +0900

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
     get 'authenticated_user_root' => 'users#index'
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-
   devise_for :photographers, path: 'photographers', controllers: {
     sessions: "photographers/sessions",
     registrations: "photographers/registrations"
@@ -16,7 +15,6 @@ Rails.application.routes.draw do
     get 'authenticated_photographer_root' => 'photographer_chat_rooms#index'
     get '/photographers/sign_out' => 'devise/sessions#destroy'
   end
-
   root 'top#index'
   resources :top, only: :index
   resources :photographers, only: :show
@@ -25,6 +23,7 @@ Rails.application.routes.draw do
       get 'match'
     end
   end
+  resources :portfolios, only: [:new, :create, :edit, :update, :destroy]
   resources :photographer_chat_rooms, only: :index
   resources :user_chat_rooms, only: :index
 end
