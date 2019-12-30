63037ef7bf6a61fe39283c4f6982581ecbd68bf9
micchel <msaito2112@gmail.com>
2019-12-30 18:18:27 +0900

    edit chatroom model

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -5,15 +5,20 @@ class Chatroom < ApplicationRecord
 
   def no_user_read_messages
     no_read_count = self.chat_messages.where.not(photographer_id: 0).where("id > ?", self.readed_photographer_message).count
-    if no_read_count != 0
-      no_read_count
+    if no_read_count > 0
+      no_read_count.to_i
+    else
+      0
     end
   end
 
   def no_photographer_read_messages
     no_read_count = self.chat_messages.where.not(user_id: 0).where("id > ?", self.readed_user_message).count
-    if no_read_count != 0
-      no_read_count
+    if no_read_count > 0
+      no_read_count.to_i
+    else
+      0
     end
   end
+
 end
