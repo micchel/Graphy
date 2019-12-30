ee1a750d69c2a714c450f1197de26cf334bdf70a
micchel <msaito2112@gmail.com>
2019-12-30 17:56:26 +0900

    edit existing controllers

--- app/controllers/photographer_chat_rooms_controller.rb
+++ app/controllers/photographer_chat_rooms_controller.rb
@@ -1,20 +1,41 @@
 class PhotographerChatRoomsController < ApplicationController
   before_action :authenticate_photographer!
+  before_action :all_no_photographer_read_messages
 
   def index
-    @chatrooms = current_photographer.chatrooms.includes(:user)
+    @chatrooms = current_photographer.chatrooms.includes(:user).eager_load(:chat_messages).order("chat_messages.id DESC")
   end
 
   def show
     @chatroom = Chatroom.find(params[:id])
-    gon.chatroom_id = @chatroom.id
-    @user = @chatroom.user
-    @chat_messages = @chatroom.chat_messages.order(:created_at).includes([:user, :photographer])
-    if @chat_messages.where.not(user_id: 0).present?
-      last_user_message = @chat_messages.where(user_id: @chatroom.user_id).last.id
-      @chatroom.update_attributes(
-        readed_user_message: last_user_message
-      )
+    if @chatroom.photographer_id == current_photographer.id 
+      gon.chatroom_id = @chatroom.id
+      @chat_messages = @chatroom.chat_messages.order(:created_at).includes([:user, :photographer])
+      if @chat_messages.where.not(user_id: 0).present?
+        last_user_message = @chat_messages.where(user_id: @chatroom.user_id).last.id
+        @chatroom.update_attributes(
+          readed_user_message: last_user_message
+        )
+      end
+    else
+      redirect_back(fallback_location: photographer_chat_rooms_path)
+    end
+  end
+
+  private
+
+  def all_no_photographer_read_messages
+    chatrooms = Chatroom.where(photographer_id: current_photographer.id)
+    messages_count = 0
+    chatrooms.each do |chatroom|
+      i = chatroom.no_photographer_read_messages
+      messages_count += i
+    end
+
+    if messages_count > 0
+      @all_no_read_messages = messages_count
+    else
+      @all_no_read_messages = 0
     end
   end
 end
--- app/controllers/photographers_controller.rb
+++ app/controllers/photographers_controller.rb
@@ -1,5 +1,6 @@
 class PhotographersController < ApplicationController
   before_action :authenticate_user!
+  before_action :all_no_user_read_messages
 
   def index
     @photographers = Reaction.where(user_id: current_user.id, status: 1).map {
@@ -11,5 +12,22 @@ class PhotographersController < ApplicationController
     @photographer = Photographer.find(params[:id])
     @chatroom = Chatroom.find_by(user_id: current_user.id, photographer_id: @photographer.id)
     @portfolios = @photographer.portfolios
+  end
+
+  private
+
+  def all_no_user_read_messages
+    chatrooms = Chatroom.where(user_id: current_user.id)
+    messages_count = 0
+    chatrooms.each do |chatroom|
+      i = chatroom.no_user_read_messages
+      messages_count += i
+    end
+
+    if messages_count > 0
+      @all_no_read_messages = messages_count
+    else
+      @all_no_read_messages = 0
+    end
   end
 end
