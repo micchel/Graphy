81fb47803c4e0f239eedd855fc3347ffd77a4192
micchel <msaito2112@gmail.com>
2020-01-06 01:28:38 +0900

    update js file
    delete console.log

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -2,18 +2,14 @@ import consumer from './consumer'
 
 $(window).on('load',function() {
   if(document.URL.match(/\/.+chat_rooms\/\d+/)) {
-    console.log("通ったで");
     var room_id = gon.chatroom_id;
-    console.log(room_id);
     var chatChannel = consumer.subscriptions.create({ channel: "ChatroomChannel", room_id: room_id }, {
       connected: function() {},
       disconnected: function() {},
       received: function(data) {
-        console.log("ここまでは来てる");
         $('.messages').append(data['content']);
       },
       speak: function(message) {
-        console.log(message);
         return this.perform('speak', {
           room_id: room_id,
           user_id: $('meta[name="current_user_id"]').attr('content'),
@@ -24,7 +20,6 @@ $(window).on('load',function() {
     });
     $(document).on('click', '#message_submit', function(event) {
       var text = $(this).prev();
-      console.log(text);
       if ($.trim(text.val()).length > 1) {
         chatChannel.speak(text.val());
         text.val('');
