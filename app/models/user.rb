edef1646c1fb2af4a278959fabf2c977ae3a8d36
micchel <msaito2112@gmail.com>
2020-01-07 00:32:09 +0900

    Edit files for user detail infomation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   has_many   :chatrooms, dependent: :destroy
   has_many   :chat_messages, dependent: :destroy
 
-  enum sex: { male:1, female:2 }
+  enum sex_type: { male:0, female:1 }
   mount_uploader :avatar, ImageUploader
 
   def have_chatroom?(photographer)
